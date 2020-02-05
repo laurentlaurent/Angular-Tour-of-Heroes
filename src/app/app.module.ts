06bb7c877b5f1fbb564a8be5d1d4615f979103c2
laurentlaurent <lao.laurent@gmail.com>
2020-02-05 13:40:32 -0500

    Separata hero edit details from hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { from } from 'rxjs';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   // Every component must be declared in exactly 1 NgModule
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
