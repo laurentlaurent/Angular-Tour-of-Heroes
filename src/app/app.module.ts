f84a5ddeaf85c42d13f2e8838099bb8cc9ede341
laurentlaurent <lao.laurent@gmail.com>
2020-02-08 00:40:13 -0500

    Fix mock and inmemory imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+// Using In-memory web api to mimick communication with a remote data server
+// In-memory Web API: https://github.com/angular/in-memory-web-api
+import { HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+// Manually import HttpClient module to enable the angular
+// mechanism for communicating with a remote server over HTTP
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,15 +21,23 @@ import { AppRoutingModule } from './app-routing.module';
   // Every component must be declared in exactly 1 NgModule
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HEROES } from './mock-heroes'; //now safe to delete thanks to in-memory-data.service
 import { MessageService } from './message.service';
 
 // @Injectable decorator marks this class as one
@@ -14,8 +15,12 @@ import { MessageService } from './message.service';
   providedIn: 'root' // from Root ensures singleton
 })
 export class HeroService {
+  // :base/:collectionName
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   // This function should be a asynchronous signature function
   // Right now, it's fine since it pulls from mock_heroes
@@ -25,6 +30,16 @@ export class HeroService {
   //   return HEROES;
   // }
 
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  // GET geroes from server
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  /* Refactored since getting heroes from HttpClient
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add(`HeroSevice: fetched heroes`);
@@ -33,9 +48,13 @@ export class HeroService {
 
   // Backtick is Javascript's template literal for embedding the id
   // Template Literal: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+   */
+
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of (HEROES.find(hero => hero.id === id));
   }
+
+
 }
